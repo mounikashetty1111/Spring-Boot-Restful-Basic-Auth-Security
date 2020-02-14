4a845579b8359c2ee15a44b06e000dfe8c41a593
atreyasarma4 <60971195+atreyasarma4@users.noreply.github.com>
2020-02-14 12:42:09 +0530

    Update Application.java

--- src/main/java/com/debu/techtalk/app/Application.java
+++ src/main/java/com/debu/techtalk/app/Application.java
@@ -2,8 +2,12 @@ package com.debu.techtalk.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan({ "com.controller.package1, com.controller.package2, com.controller.package3, com.controller.packageN", "controller", "service" } // If our Controller class or Service class is not in the same packages we have //to add packages's name like this...directory(package) with main class
+
 public class Application {
 
 	public static void main(String[] args) {
